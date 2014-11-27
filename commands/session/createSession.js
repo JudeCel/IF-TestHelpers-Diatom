0d1d7bffa70e2cf1ea6950bfe1f7d3ac27fa3cb4
lilutanya <lilu.tanya@gmail.com>
2014-11-28 01:19:41 +0200

    Fix createSessionMethod

--- commands/session/createSession.js
+++ commands/session/createSession.js
@@ -2,19 +2,61 @@
 var _ = require('lodash');
 var ifCommon = require('if-common');
 var mtypes = ifCommon.mtypes;
-var ifData = require('if-data');
 var Q = require('q');
+var ifData = require('if-data'), db = ifData.db;
 
 module.exports = function (params) {
-	var uuid = ifCommon.utils.uuidHelper.generateUUID();
-	var newSession = _.defaults(_.clone(params || {}), {
-		name: uuid,
-		status_id: mtypes.statusLookup.active,
-		start_time: '2014-1-1',
-		end_time: '2016-1-1'
-	});
 
-	return Q.nfcall(ifData.db.insert, "sessions", newSession).then(function() {
-		return _.omit(newSession, "insertId");
-	});
+	return getUntitledSessionNames({accountId: params.accountId})
+		.then(calcSessionName)
+		.then(function (sessionName) {
+			return createSession({
+				accountId: params.accountId,
+				name: sessionName,
+				status_id: mtypes.sessionStatus.pending
+			});
+		});
 };
+
+
+function getUntitledSessionNames(params) {
+	var accountId = params.accountId;
+	var sql = "SELECT \
+			s.name \
+		FROM sessions s \
+		WHERE s.accountId = ? \
+		AND s.deleted IS NULL \
+		AND s.name LIKE '%Untitled Session%' \
+		ORDER BY s.name";
+
+	return Q.nfcall(db.query, sql, [accountId])
+		.then(function (results) {
+			return _.pluck(results, 'name');
+		});
+}
+
+function calcSessionName(names) {
+	var nameRegex = /(Copy )(\d+)( of )/;
+
+	if (_.isEmpty(names)) return 'Untitled Session';
+
+	var getCopyNum = function(name) {
+		if (!nameRegex.test(name)) return 0;
+
+		var matches = nameRegex.exec(name);
+		if(!matches || !matches[2]) return 0;
+
+		return parseInt(matches[2], 10);
+	};
+
+	var highestCopyNum = _(names).map(getCopyNum).max().valueOf();
+	highestCopyNum++;
+	return 'Copy ' + highestCopyNum + ' of Untitled Session';
+}
+
+function createSession(params) {
+	return Q.nfcall(db.insert, "sessions", params).then(function () {
+		return _.omit(params, 'insertId', 'created', 'modified')
+	});
+}
+
